65412e46976aa7e032d440ef4990330232e7b9f0
kitaya <tomozho1999@icloud.com>
2023-05-23 20:24:45 +0900

    add vercel.json

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,7 @@ app.use(cors());
 app.use("/api", router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).send("500 Internal Server Error");
+  res.status(500).send(err.message || "500 Internal Server Error");
 });
 
 const port = process.env.SERVER_PORT || 8000;
