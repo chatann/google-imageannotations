2198fb13e1ad3c8d103d2ce0ae32732066fc66e3
kitaya <tomozho1999@icloud.com>
2023-05-24 16:11:33 +0900

    change handling route

--- server/utils/router.ts
+++ server/utils/router.ts
@@ -1,22 +1,27 @@
 import { Router, Request, Response } from "express";
-import multer from "multer";
 import uploadS3 from "./uploadS3";
 import getResultFromS3 from "./getResultFromS3";
 
 const router = Router();
 
-router.post("/upload", (req, res) => {
-  uploadS3(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-      return res.status(400).json({ error: err.message });
-    } else if (err) {
-      return res.status(500).json({ error: "Something went wrong" });
-    }
+router.post(
+  "/upload",
+  (req, res, next) => {
+    uploadS3(req, res, (err: any) => {
+      if (err) {
+        return res.status(400).json({ err: err.message });
+      }
+      next();
+    });
+  },
+  async (req, res) => {
     const reqFile = req.file as Express.MulterS3.File;
     const objectKey = reqFile.key;
-    const result = getResultFromS3(objectKey);
+
+    const result = await getResultFromS3(objectKey);
+
     return res.status(200).json(result);
-  });
-});
+  }
+);
 
 export default router;
