48b6af866a9285b051ee03125a757fdafc9d6490
kitaya <tomozho1999@icloud.com>
2023-05-24 17:28:40 +0900

    fix error handling

--- server/utils/getResultFromS3.ts
+++ server/utils/getResultFromS3.ts
@@ -3,26 +3,32 @@ import { GetObjectCommand, HeadObjectCommand } from "@aws-sdk/client-s3";
 import { awsBucketName } from "../config/config";
 
 const getResultFromS3 = async (objectKey: string) => {
-  const params = {
-    Bucket: awsBucketName,
-    Key: objectKey,
-  };
-  const headObjectCommand = new HeadObjectCommand(params);
+  return new Promise<string>((resolve, reject) => {
+    const params = {
+      Bucket: awsBucketName,
+      Key: objectKey,
+    };
+    const headObjectCommand = new HeadObjectCommand(params);
 
-  const interval = setInterval(async () => {
-    try {
-      const head = await s3.send(headObjectCommand);
-      if (head.ContentLength) {
-        clearInterval(interval);
-        const getObjectCommand = new GetObjectCommand(params);
-        const data = await s3.send(getObjectCommand);
-        const result = data.Body?.toString();
-        return result;
+    const interval = setInterval(async () => {
+      try {
+        const head = await s3.send(headObjectCommand);
+        if (head.ContentLength) {
+          clearInterval(interval);
+          const getObjectCommand = new GetObjectCommand(params);
+          const data = await s3.send(getObjectCommand);
+          const result = data.Body?.toString();
+          if (!result) {
+            throw new Error("No result found");
+          }
+          resolve(result);
+        }
+      } catch (err) {
+        console.log("Error retrieving result from S3:", err);
+        reject(err);
       }
-    } catch (err) {
-      console.log("Error retrieving result from S3:", err);
-    }
-  }, 1000);
+    }, 1000);
+  });
 };
 
 export default getResultFromS3;
